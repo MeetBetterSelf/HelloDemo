184b7f4f84b2c94060f12205caa29a3875c4ed20
yan <3353650143@qq.com>
2021-06-20 17:14:51 +0800

    新增Controller

---
+++ src/main/java/com/yan/demo/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.yan.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 类功能描述：
+ *
+ * @author：yandehong
+ * @createTime：2021/6/20 17:13
+ */
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Index Page.";
+    }
+}
