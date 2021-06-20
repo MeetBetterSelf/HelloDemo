37040b704f59ebdbf7d11ceec04f21e063f2d7e2
yan <3353650143@qq.com>
2021-06-20 16:47:24 +0800

    初始化

---
+++ src/main/java/com/yan/demo/HelloDemoApplication.java
@@ -0,0 +1,19 @@
+package com.yan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 类功能描述：
+ *
+ * @author：yandehong
+ * @createTime：2021/6/20 16:28
+ */
+@SpringBootApplication
+public class HelloDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloDemoApplication.class, args);
+    }
+
+}
